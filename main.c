26ce48fb1bcfc9566264a3b3397fffb217d07fc8
jevian ma(马作伟) <jevian_ma@126.com>
2018-10-11 20:32:31 +0800

    去掉不必要的log

--- main.c
+++ main.c
@@ -51,7 +51,6 @@ int connectionHandler(void *cls,
             MHD_add_response_header(response, "Access-Control-Allow-Headers", "*");
         }
     } else {
-        printf ("url:%s, in %s, at %d\n", url, __FILE__, __LINE__);
         size_t size = strlen(url);
         if (!strcmp(url + size-3, ".ts")) {
             char *topic = (char*)memalloc(size-3, __FILE__, __LINE__); // "x.ts"共4个字符,不减4只减3是为了\0
