a43d448dac34e04cf41370d56521d3562882d8cf
jevian ma(马作伟) <jevian_ma@126.com>
2018-10-17 00:08:41 +0800

    修改配置文件参数

--- config.c
+++ config.c
@@ -14,20 +14,32 @@ struct CONFIG* initconfig () {
     if(!access("config.json", F_OK)) {
         json_error_t error;
         json_t *obj = json_load_file("config.json", 0, &error);
-        config.tsdatabuffsize = json_integer_value(json_object_get(obj, "tsdatabuffsize"));
-        config.port = json_integer_value(json_object_get(obj, "port"));
-        config.tstimelong = json_integer_value(json_object_get(obj, "tstimelong"));
+        json_t *tsdatabuffsize = json_object_get(obj, "tsdatabuffsize");
+        if (tsdatabuffsize == NULL)
+            config.tsdatabuffsize = 512*1024;
+        else
+            config.tsdatabuffsize = json_integer_value(tsdatabuffsize);
+        json_t *port = json_object_get(obj, "port");
+        if (port == NULL)
+            config.port = 8002;
+        else
+            config.port = json_integer_value(port);
+        json_t *tstimelong = json_object_get(obj, "tstimelong");
+        if (port == NULL)
+            config.tstimelong = 1000000;
+        else
+            config.tstimelong = json_integer_value(tstimelong);
         json_decref(obj);
     } else {
         json_t *obj = json_object();
         json_object_set_new(obj, "tsdatabuffsize", json_integer(1024*1024));
         json_object_set_new(obj, "port", json_integer(8002));
-        json_object_set_new(obj, "tstimelong", json_integer(500000));
+        json_object_set_new(obj, "tstimelong", json_integer(1000000));
         json_dump_file(obj, "config.json", 0);
         json_decref(obj);
         config.tsdatabuffsize = 512*1024;
         config.port = 8002;
-        config.tstimelong = 500000;
+        config.tstimelong = 1000000;
     }
     return &config;
 }
